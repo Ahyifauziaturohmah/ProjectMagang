5ba7a7e607323d40b61ab636afbe7b97ea678c22
zia <fauziaturohmah.hyi@gmail.com>
2026-01-21 13:55:58 +0700

    fixx1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
-  
   plugins: [],
 }
